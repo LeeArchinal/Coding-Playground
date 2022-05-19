7005263dc69a452bde2d0745add9ffc1be0114dd
LeeArchinal <74731739+LeeArchinal@users.noreply.github.com>
2022-05-19 11:01:44 -0400

    Initial push of Proposal.py

---
+++ Proposal.py
@@ -0,0 +1,8 @@
+print("Will you marry me?")
+answer = input("Yes or No:")
+if answer.lower() == 'no':
+    print('Well shit.')
+if answer.lower() == 'yes':
+    print('Thank you for making me the happiest person alive!')
+else:
+    print('Say again!?')
